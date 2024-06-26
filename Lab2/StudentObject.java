174c7a6217f0b07edfabcc5dce67512602c05fd3
Ishwor <dulalishan3@gmail.com>
2024-06-26 11:46:46 +0545

    add Student child class
    Co-authored-by: Ishwor <dulalishan3@gmail.com>
    Co-authored-by: ish-py <dulalishan13@gmail.com>

---
+++ Lab2/StudentObject.java
@@ -0,0 +1,9 @@
+package ChapterThree.Lab2;
+public class StudentObject {
+    public static void main(String[] args) {
+        Student newStudent = new Student(6,"ish", "dhapakhel");
+        System.out.println(newStudent.name);
+        System.out.println(newStudent.rollNo);
+        System.out.println(newStudent.address);
+    }
+}
