506a4f723bca13f532e9fa08f8be7017d2bc3333
Ishwor <dulalishan3@gmail.com>
2024-06-27 15:43:13 +0545

    add parent class Car
    Co-authored-by: Ishwor <dulalishan3@gmail.com>
    Co-authored-by: ish-py <dulalishan13@gmail.com>

---
+++ Lab4/Car.java
@@ -0,0 +1,20 @@
+package ChapterThree.Lab4;
+public class Car{
+    String modelName;
+    public int price;
+    public void start(){
+
+    }
+    public static void main(String[] args){
+
+        Car tesla = new Car();
+//        tesla.price = 100000;
+//        tesla.modelName = "prashant gay";
+        Car lamborghini = new Car();
+//        lamborghini.price = 200009870;
+        System.out.println(tesla.modelName = "Prashant nigga");
+        System.out.println(tesla.price = 8913211);
+        System.out.println(lamborghini.price = 39239988);
+    }
+
+}
